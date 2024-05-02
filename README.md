1923d8ac5d9ce6096be7b684bd60825233258a79
hfxz <xuan__zai@outlook.com>
2024-05-02 20:45:06 +0800

    neovim 基础配置

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 我的配置文件，使用 [Dotbot](https://github.com/anishathalye/dotbot?tab=readme-ov-file) 管理
 
+neovim 使用 [LazyVim](https://github.com/LazyVim/LazyVim)
---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,16 @@
+-- 准备 lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- 加载插件
+require("lazy").setup({{import = "plugins"}})
