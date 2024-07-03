9107950322140d2c2f68ef60d764887adaab4d2a
hfxz <xuan__zai@outlook.com>
2024-07-03 15:50:59 +0800

    update

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,8 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
 local opt = vim.opt
 
 opt.wrap = true
 opt.scrolloff = 5
 opt.spelllang = { "en", "cjk" }
+
+-- If no prettier config file is found, the formatter will not be used
+vim.g.lazyvim_prettier_needs_config = true
