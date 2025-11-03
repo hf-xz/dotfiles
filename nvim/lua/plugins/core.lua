a7750d69b3aeb8fdb2be46d7383e39fcb26247e4
hfxz <xuan__zai@outlook.com>
2025-11-03 16:12:24 +0800

    update lazyvim config

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -8,12 +8,15 @@ return {
   -- LSP keymaps
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      -- disable a keymap
-      keys[#keys + 1] = { "K", false }
-      -- add a keymap
-      keys[#keys + 1] = { "gh", vim.lsp.buf.hover, desc = "Hover" }
-    end,
+    opts = {
+      servers = {
+        ["*"] = {
+          keys = {
+            { "gh", "<cmd>lua vim.lsp.buf.hover()<CR>", has = "Hover" },
+            { "K", false },
+          },
+        },
+      },
+    },
   },
 }
