1715c8223eee9c64b03e9b651135852cceb971a1
hfxz <xuan__zai@outlook.com>
2025-12-14 10:09:18 +0800

    format wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,12 +4,16 @@ local config = wezterm.config_builder()
 
 config = {
 	automatically_reload_config = true,
+
 	enable_tab_bar = false,
 	window_close_confirmation = "NeverPrompt",
 	window_decorations = "RESIZE",
+
 	color_scheme = "Catppuccin Mocha", -- Mocha, Macchiato, Frappe, Latte
+
 	font = wezterm.font("Maple Mono NF CN"),
 	font_size = 14,
+
 	window_background_opacity = 0.92,
 	macos_window_background_blur = 10,
 }
